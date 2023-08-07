1592a4342646e46cac5469a9b226ce3f8b8218fc
kirillabcd <k.motsart@gmail.com>
2023-08-08 01:41:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    //1.first loop cut expression for 10 symbols
+    for (let i = 0; i < expr.length; i+= 10) {
+        let sliceTen = expr.slice(i, i + 10);
+        //2.if word = 10* decode as space symbol
+        if (sliceTen === '**********') {
+            result += ' ';
+        } else {
+            let morseKey ='';
+            //3. second loop cut sliceTen for 2 symbols 1 or 0 to convert to + or -
+            for (let j = 0; j < 10; j+= 2) {
+                let sliceTwo = sliceTen.slice(j, j + 2);
+
+                //4. If two symbols = 10 convert to '.' and add to morseKey
+                if (sliceTwo === '10') {
+                    morseKey += '.';
+                //5. If two symbols = 11 convert to '.' and add to morseKey
+                } else if (sliceTwo === '11') {
+                    morseKey += '-';
+                }
+            }
+            //6. add to result value of morseKey
+            result += MORSE_TABLE[morseKey];
+        }
+    }
+    return result;
 }
 
 module.exports = {
